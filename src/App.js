aeeb515026026b928b13599ccdbd68887cc0be80
Shokupanman0I <shokupanman4real@gmail.com>
2022-04-27 08:26:34 +0900

    call back hell added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={badFunc}>BAD BUTTON</button>
+        <button onClick={badLoopFunc}>BAD LOOP BUTTON</button>
+        <button onClick={callBackHell}>CALL BACK HELL BUTTON</button>
       </header>
     </div>
   );
@@ -14,19 +15,36 @@ function App() {
 
 let badVar = false;
 let loopNum = 0;
+let badState = "bad";
 
-let badFunc = () => {
+let badLoopFunc = () => {
 
   if(!badVar)
   {
     console.log(`THE LOOP ${loopNum}`)
     loopNum++
-    badFunc()
+    badLoopFunc()
   }
   else{
     return
   }
 
+}
+
+
+let callBackHell = () => {
+  setTimeout(function() {
+    console.log('getting worse');
+    badState = "worse"
+    setTimeout(function() {
+      console.log('oh no');
+      badState = "even worse"
+      setTimeout(function() {
+        console.log('AHHHHHHHHHHHHHHHHHHH');
+        badState = "worst"
+      }, 2000);
+    }, 2000);
+  }, 2000);
 }
 
 export default App;
